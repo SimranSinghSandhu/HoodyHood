f06047ee9ced5ae643851da765fccbbd44bb1398
Simran <simran@dev-story.com>
2025-05-19 18:50:36 +0530

    Added app icon

---
+++ HoodyHood/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  HoodyHood
+//
+//  Created by Simran Sandhu on 19/05/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    LoginView()
+}
